3ea0628b90d4525646e3402c79e272aa5515f4f0
Drew Proud <therealdrewproud@gmail.com>
2017-04-18 18:43:52 -0400

    Added jest and first test

---
+++ src/components/ChatBox/index.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+class ChatBox extends Component {
+  render() {
+    return <div />;
+  }
+}
+
+export default ChatBox;
---
+++ src/components/ChatBox/spec.js
@@ -0,0 +1,7 @@
+import ChatBox from './index';
+
+describe('ChatBox', () => {
+  it('is defined', () => {
+    expect(ChatBox).toBeDefined();
+  });
+});
