805270473d3629e949f4232a8ea9cb0eb526cf3e
Drew Proud <therealdrewproud@gmail.com>
2017-04-18 18:37:58 -0400

    Added new dependencies

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import React, { Component } from 'react';
 
-export default class App extends Component {
+class App extends Component {
   render() {
     return (
-      <h1>Hello, world.</h1>
+      <div>
+        <h1>Welcome to my chat app</h1>
+      </div>
     );
   }
 }
+
+export default App;
